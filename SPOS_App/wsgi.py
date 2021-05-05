cfe592acf80c28449ba98666ac7328d3a507d85a
kris <kristopherc@codev.com>
2021-05-05 22:36:36 +0800

    update wrong folder defined

--- SPOS_App/wsgi.py
+++ SPOS_App/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SPOS_App_Django project.
+WSGI config for SPOS_App project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SPOS_App_Django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SPOS_App.settings')
 
 application = get_wsgi_application()
